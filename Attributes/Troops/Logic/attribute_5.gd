ff22fd8aa22d8fe77717dc1348a6223d437a7c7c
tealeaf2 <shadowrocket2003@gmail.com>
2024-04-24 12:47:46 -0400

    Made edits to resolve requested changes

--- Attributes/Troops/Logic/attribute_5.gd
+++ Attributes/Troops/Logic/attribute_5.gd
@@ -11,19 +11,20 @@ func build_action() -> Action:
 
 #loops to all tiles around defender and applies half damage
 
-func on_attack(defender: Unit):
+func on_attack(defender: Unit, attacker: Unit):
     var defender_pos = defender.pos
-    var splash_tiles = _get_surrounding(defender_pos, 1)
+    var splash_tiles = defender._get_surrounding(defender_pos, 1)
 
     for tile in splash_tiles:
-        var unit_at_tile = game.board.units[tile.x][tile.y]
-        apply_splash_damage(unit_at_tile)
+        var unit_at_tile = board.units[tile.x][tile.y]
+        if unit_at_tile != null:
+            apply_splash_damage(unit_at_tile, attacker)
 
 #Applies half attack damage to enemy
 
-func apply_splash_damage(troop: Troop):
-    var splash_damage = floor(base_stats.attack / 2.0)
-    troop.health -= splash_damage
-    if troop.health <= 0:
-        troop.health = 0
-        game.remove_unit(troop)
+func apply_splash_damage(troop_defender: Troop, troop_attacker: Troop):
+    var splash_damage = floor(troop_attacker.attack / 2.0)
+    troop_defender.health -= splash_damage
+    if troop_defender.health <= 0:
+        troop_defender.health = 0
+        parent.game.remove_unit(troop_defender)
