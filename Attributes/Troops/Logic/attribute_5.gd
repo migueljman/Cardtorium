1374625f96c1af57447edc080603c959e02b8483
tealeaf2 <shadowrocket2003@gmail.com>
2024-04-28 23:36:02 -0400

    implemented new splash damage calculations

--- Attributes/Troops/Logic/attribute_5.gd
+++ Attributes/Troops/Logic/attribute_5.gd
@@ -12,10 +12,14 @@ func on_attack(defender: Unit, attacker: Unit):
         if unit_at_tile != null:
             apply_splash_damage(unit_at_tile, attacker)
 
-#Applies half attack damage to enemy
+#Applies splash damage to enemy
 
 func apply_splash_damage(troop_defender: Troop, troop_attacker: Troop):
-    var splash_damage = floor(troop_attacker.attack / 2.0)
+    var atk_force  = troop_attacker.attack * float(troop_attacker.current_health)/float(troop_attacker.max_health)
+    var def_force = troop_defender.defense * float(troop_defender.current_health)/float(troop_defender.max_health)
+    var damage = floor((atk_force / (atk_force+def_force)) * troop_attacker.attack)
+    var splash_damage = floor(damage / 2.0)
+
     troop_defender.health -= splash_damage
     if troop_defender.health <= 0:
         troop_defender.health = 0
